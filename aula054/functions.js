c0ec75591c6d699ad6b565e986efb14f19f8c4cc
Godoy <j.godoysoares@gmail.com>
2022-01-28 00:13:50 -0300

    nivel 2 do jogo

--- aula054/functions.js
+++ aula054/functions.js
@@ -3,9 +3,10 @@ var tabuleiro = [];
 var quemJoga = 0;//0 vai ser o jogador --- 1 vai ser cpu
 var verifica;
 var jogando = true;
-var nivel = 1;
+var nivel = 2;
 var jogadaCpu = 1;
 var quemComeca = 1;
+var jogada = 0;
 
 function cpuJoga() {
     if (jogando) {
@@ -17,7 +18,152 @@ function cpuJoga() {
             } while (jogo[l][c] != "");
             jogo[l][c] = "O";
         } else if (nivel == 2) {
-            //Nível2
+            //ATAQUE
+            //jogadas linha 1
+            if ((jogo[0][0] == "O") && (jogo[0][1] == "O") && (jogo[0][2] == "")) {
+                jogo[0][2] = "O";
+            } else if ((jogo[0][0] == "O") && (jogo[0][2] == "O") && (jogo[0][1] == "")) {
+                jogo[0][1] = "O";
+            } else if ((jogo[0][1] == "O") && (jogo[0][2] == "O") && (jogo[0][0] == "")) {
+                jogo[0][0] = "O";
+            } else
+                //jogadas linhas 2
+                if ((jogo[1][0] == "O") && (jogo[1][1] == "O") && (jogo[1][2] == "")) {
+                    jogo[1][2] = "O";
+                } else if ((jogo[1][0] == "O") && (jogo[1][2] == "O") && (jogo[1][1] == "")) {
+                    jogo[1][1] = "O";
+                } else if ((jogo[1][1] == "O") && (jogo[1][2] == "O") && (jogo[1][0] == "")) {
+                    jogo[10][0] = "O";
+                } else
+                    //jogadas linha 3
+                    if ((jogo[2][0] == "O") && (jogo[2][1] == "O") && (jogo[2][2] == "")) {
+                        jogo[2][2] = "O";
+                    } else if ((jogo[2][0] == "O") && (jogo[2][2] == "O") && (jogo[2][1] == "")) {
+                        jogo[2][1] = "O";
+                    } else if ((jogo[2][1] == "O") && (jogo[2][2] == "O") && (jogo[2][0] == "")) {
+                        jogo[2][0] = "O";
+                    } else
+                        //jogadas coluna 1
+                        if ((jogo[0][0] == "O") && (jogo[1][0] == "O") && (jogo[2][0] == "")) {
+                            jogo[2][0] = "O";
+                        } else if ((jogo[0][0] == "O") && (jogo[2][0] == "O") && (jogo[1][0] == "")) {
+                            jogo[1][0] = "O";
+                        } else if ((jogo[1][0] == "O") && (jogo[2][0] == "O") && (jogo[0][0] == "")) {
+                            jogo[0][0] = "O";
+                        } else
+                            //jogadas coluna 2
+                            if ((jogo[0][1] == "O") && (jogo[1][1] == "O") && (jogo[2][1] == "")) {
+                                jogo[2][1] = "O";
+                            } else if ((jogo[0][1] == "O") && (jogo[2][1] == "O") && (jogo[1][1] == "")) {
+                                jogo[1][1] = "O";
+                            } else if ((jogo[1][1] == "O") && (jogo[2][1] == "O") && (jogo[0][1] == "")) {
+                                jogo[0][1] = "O";
+                            } else
+                                //jogadas coluna 3
+                                if ((jogo[0][2] == "O") && (jogo[1][2] == "O") && (jogo[2][2] == "")) {
+                                    jogo[2][2] = "O";
+                                } else if ((jogo[0][2] == "O") && (jogo[2][2] == "O") && (jogo[1][2] == "")) {
+                                    jogo[1][2] = "O";
+                                } else if ((jogo[1][2] == "O") && (jogo[2][2] == "O") && (jogo[0][2] == "")) {
+                                    jogo[0][2] = "O";
+                                } else
+                                    //jogadas diagonal 1
+                                    if ((jogo[0][0] == "O") && (jogo[1][1] == "O") && (jogo[2][2] == "")) {
+                                        jogo[2][2] = "O";
+                                    } else if ((jogo[0][0] == "O") && (jogo[2][2] == "O") && (jogo[1][1] == "")) {
+                                        jogo[1][1] = "O";
+                                    } else if ((jogo[1][1] == "O") && (jogo[2][2] == "O") && (jogo[0][0] == "")) {
+                                        jogo[0][0] = "O";
+                                    } else
+                                        //jogadas diagonal 2
+                                        if ((jogo[0][2] == "O") && (jogo[1][1] == "O") && (jogo[2][0] == "")) {
+                                            jogo[2][0] = "O";
+                                        } else if ((jogo[0][2] == "O") && (jogo[2][0] == "O") && (jogo[1][1] == "")) {
+                                            jogo[1][1] = "O";
+                                        } else if ((jogo[2][0] == "O") && (jogo[1][1] == "O") && (jogo[0][2] == "")) {
+                                            jogo[0][2] = "O";
+                                        } else
+                                            //DEFESA
+                                            //jogadas linha 1
+                                            if ((jogo[0][0] == "X") && (jogo[0][1] == "X") && (jogo[0][2] == "")) {
+                                                jogo[0][2] = "O";
+                                            } else if ((jogo[0][0] == "X") && (jogo[0][2] == "X") && (jogo[0][1] == "")) {
+                                                jogo[0][1] = "O";
+                                            } else if ((jogo[0][1] == "X") && (jogo[0][2] == "X") && (jogo[0][0] == "")) {
+                                                jogo[0][0] = "O";
+                                            } else
+                                                //jogadas linha 2
+                                                if ((jogo[1][0] == "X") && (jogo[1][1] == "X") && (jogo[1][2] == "")) {
+                                                    jogo[1][2] = "O";
+                                                } else if ((jogo[1][0] == "X") && (jogo[1][2] == "X") && (jogo[1][1] == "")) {
+                                                    jogo[1][1] = "O";
+                                                } else if ((jogo[1][1] == "X") && (jogo[1][2] == "X") && (jogo[1][0] == "")) {
+                                                    jogo[1][0] = "O";
+                                                } else
+                                                    //jogadas linha 3
+                                                    if ((jogo[2][0] == "X") && (jogo[2][1] == "X") && (jogo[2][2] == "")) {
+                                                        jogo[2][2] = "O";
+                                                    } else if ((jogo[2][0] == "X") && (jogo[2][2] == "X") && (jogo[2][1] == "")) {
+                                                        jogo[2][1] = "O";
+                                                    } else if ((jogo[2][1] == "X") && (jogo[2][2] == "X") && (jogo[2][0] == "")) {
+                                                        jogo[2][0] = "O";
+                                                    } else
+                                                        //jogadas coluna 1
+                                                        if ((jogo[0][0] == "X") && (jogo[1][0] == "X") && (jogo[2][0] == "")) {
+                                                            jogo[2][0] = "O";
+                                                        } else if ((jogo[0][0] == "X") && (jogo[2][0] == "X") && (jogo[1][0] == "")) {
+                                                            jogo[0][1] = "O";
+                                                        } else if ((jogo[1][0] == "X") && (jogo[2][0] == "X") && (jogo[0][0] == "")) {
+                                                            jogo[0][0] = "O";
+                                                        } else
+                                                            //jogadas coluna 2
+                                                            if ((jogo[0][1] == "X") && (jogo[1][1] == "X") && (jogo[2][1] == "")) {
+                                                                jogo[2][1] = "O";
+                                                            } else if ((jogo[0][1] == "X") && (jogo[2][1] == "X") && (jogo[1][1] == "")) {
+                                                                jogo[1][1] = "O";
+                                                            } else if ((jogo[1][1] == "X") && (jogo[2][1] == "X") && (jogo[0][1] == "")) {
+                                                                jogo[0][1] = "O";
+                                                            } else
+                                                                //jogadas coluna 3
+                                                                if ((jogo[0][2] == "X") && (jogo[1][2] == "X") && (jogo[2][2] == "")) {
+                                                                    jogo[2][2] = "O";
+                                                                } else if ((jogo[0][2] == "X") && (jogo[2][2] == "X") && (jogo[1][2] == "")) {
+                                                                    jogo[1][2] = "O";
+                                                                } else if ((jogo[1][2] == "X") && (jogo[2][2] == "X") && (jogo[0][2] == "")) {
+                                                                    jogo[0][2] = "O";
+                                                                } else
+                                                                    //jogadas diagonal 1
+                                                                    if ((jogo[0][0] == "X") && (jogo[1][1] == "X") && (jogo[2][2] == "")) {
+                                                                        jogo[2][2] = "O";
+                                                                    } else if ((jogo[0][0] == "X") && (jogo[2][2] == "X") && (jogo[1][1] == "")) {
+                                                                        jogo[1][1] = "O";
+                                                                    } else if ((jogo[1][1] == "X") && (jogo[2][2] == "X") && (jogo[0][0] == "")) {
+                                                                        jogo[0][0] = "O";
+                                                                    } else
+                                                                        //jogdas diagonal 2
+                                                                        if ((jogo[0][2] == "X") && (jogo[1][1] == "X") && (jogo[2][0] == "")) {
+                                                                            jogo[2][0] = "O";
+                                                                        } else if ((jogo[0][2] == "X") && (jogo[2][0] == "X") && (jogo[1][1] == "")) {
+                                                                            jogo[1][1] = "O";
+                                                                        } else if ((jogo[2][0] == "X") && (jogo[1][1] == "X") && (jogo[0][2] == "")) {
+                                                                            jogo[0][2] = "O";
+                                                                        } else {
+                                                                            if (jogada < 8) {
+                                                                                do {
+                                                                                    l = Math.round(Math.random() * 2);
+                                                                                    c = Math.round(Math.random() * 2);
+                                                                                } while (jogo[l][c] != "");
+                                                                                jogo[l][c] = "O";
+                                                                            } else {
+                                                                                for (var l = 0; l < 3; l++) {
+                                                                                    for (var c = 0; c < 3; c++) {
+                                                                                        if (jogo[l][c] == "") {
+                                                                                            jogo[l][c] = "O";
+                                                                                        }
+                                                                                    }
+                                                                                }
+                                                                            }
+                                                                        }
         }
         verifica = verificaVitoria();
         if (verifica != "") {
@@ -25,6 +171,7 @@ function cpuJoga() {
             jogando = false;
         }
         atualizaTabuleiro();
+        jogada++;
         quemJoga = 0;
     }
 }
@@ -120,6 +267,7 @@ function jogar(p) {
                 alert(verifica + " venceu");
                 jogando = false;
             }
+            jogada++;
             cpuJoga();
         }
     }
@@ -145,6 +293,7 @@ function atualizaTabuleiro() {
 function inicia() {
     jogando = true;
     jogadaCpu = 1;
+    jogada = 0;
     jogo = [
         ["", "", ""],
         ["", "", ""],
@@ -155,6 +304,17 @@ function inicia() {
         [document.getElementById('p4'), document.getElementById('p5'), document.getElementById('p6')],
         [document.getElementById('p7'), document.getElementById('p8'), document.getElementById('p9')]
     ];
+    atualizaTabuleiro();
+    if (quemComeca == 1) {
+        quemComeca = 0;
+        quemJoga = quemComeca;
+        document.getElementById("dvQuemComeca").innerHTML = "Quem começa: Jogador";
+    } else {
+        quemComeca = 1;
+        quemJoga = quemComeca;
+        cpuJoga();
+        document.getElementById("dvQuemComeca").innerHTML = "Quem começa: CPU";
+    }
 }
 
 inicia();
