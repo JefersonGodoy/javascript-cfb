43bda4eaf540221d661773f1ed2a32e3d0efde65
Godoy <j.godoysoares@gmail.com>
2022-01-29 13:29:26 -0300

    informando as variaveis

---
+++ aula057/script.js
@@ -0,0 +1,32 @@
+//Elementos
+var vBtIniciar, vBola, vCpu, vJogador, vPaineltxtpontos;
+
+//Controle de animação
+var game, frames;
+
+//Posições
+var posBolaX, posBolaY;
+var posJogadorY;
+var posCpuX, posCpuY;
+
+//Direção de acordo com a tela
+var dirJy;
+
+//Posições iniciais
+var posJogIniY = 180, posCpuIniY = 180;
+var posBolaIniX = 475, posBolaIniy = 240;
+
+//Tamanhos
+var campoX = 0, campoY = 0, campoW = 960, campoH = 500;
+var barraW = 20, barraH = 140, bolaW = 20, bolaH = 20;
+
+//Direção
+var bolaX, bolaY;
+var jogadorY = 0, cpuY = 0;
+
+//Velocidade
+var velBola, velCpu, velJogador;
+
+//Controle
+var pontos = 0;
+var tecla;
