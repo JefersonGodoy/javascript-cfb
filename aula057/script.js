b23772a6e1691585d7aa5f37801ebd5bf5cb8375
Godoy <j.godoysoares@gmail.com>
2022-01-31 21:38:24 -0300

    iniciando as funções

--- aula057/script.js
+++ aula057/script.js
@@ -7,7 +7,7 @@ var game, frames;
 //Posições
 var posBolaX, posBolaY;
 var posJogadorY;
-var posCpuX, posCpuY;
+var posCpuY;
 
 //Direção de acordo com a tela
 var dirJy;
@@ -22,7 +22,7 @@ var barraW = 20, barraH = 140, bolaW = 20, bolaH = 20;
 
 //Direção
 var bolaX, bolaY;
-var jogadorY = 0, cpuY = 0;
+var cpuY = 0;
 
 //Velocidade
 var velBola, velCpu, velJogador;
@@ -30,3 +30,48 @@ var velBola, velCpu, velJogador;
 //Controle
 var pontos = 0;
 var tecla;
+var jogo = false
+
+function teclaDw() {
+    tecla = event.keyCode;
+}
+
+function teclaUp() {
+
+}
+
+function game() {
+    if (jogo) {
+
+    }
+
+    frames = requestAnimationFrame(game);
+}
+
+function iniciaJogo() {
+    if (!jogo) {
+        cancelAnimationFrame(frames);
+        jogo = true;
+        dirJy = 0;
+        posBolaX = posBolaIniX;
+        posBolaY = posBolaIniy;
+        posJogadorY = 0;
+        posCpuY = posCpuIniY;
+        game();
+    }
+
+}
+
+function inicializa() {
+    vBtIniciar = document.getElementById("btIniciar");
+    vBtIniciar.addEventListener("click", iniciaJogo);
+    vJogador = document.getElementById("dvJogador");
+    vCpu = document.getElementById("dvCpu");
+    vBola = document.getElementById("dvBola");
+    vPaineltxtpontos = document.getElementById("txtPontos");
+    document.addEventListener("keydown", teclaDw);
+    document.addEventListener("keydown", teclaUp);
+}
+
+
+window.addEventListener("load", inicializa)
