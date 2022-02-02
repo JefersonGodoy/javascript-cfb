0291779013842f4ddea6d07d4e791008f4163700
Godoy <j.godoysoares@gmail.com>
2022-02-01 23:44:47 -0300

    movimentando a bola

--- aula057/script.js
+++ aula057/script.js
@@ -6,15 +6,15 @@ var game, frames;
 
 //Posições
 var posBolaX, posBolaY;
-var posJogadorY;
-var posCpuY;
+var posJogadorX, posJogadorY;
+var posCpuX, posCpuY;
 
 //Direção de acordo com a tela
 var dirJy;
 
 //Posições iniciais
 var posJogIniY = 180, posCpuIniY = 180;
-var posBolaIniX = 475, posBolaIniy = 240;
+var posBolaIniX = 475, posBolaIniY = 240;
 
 //Tamanhos
 var campoX = 0, campoY = 0, campoW = 960, campoH = 500;
@@ -32,11 +32,45 @@ var pontos = 0;
 var tecla;
 var jogo = false;
 
-function controlaJog() {
+function controlaJogo() {
     if (jogo) {
         posJogadorY += velJogador * dirJy;
+        if (
+            ((posJogadorY + barraH) >= campoH) || ((posJogadorY) <= 0)
+        ) {
+            posJogadorY += (velJogador * dirJy) * (-1);
+        }
         vJogador.style.top = posJogadorY + "px";
     }
+}
+
+function controlaBola() {
+    //Movimentação da bola
+    posBolaX += velBola * bolaX;
+    posBolaY += velBola * bolaY;
+
+    //Colisão com o jogador
+    if (
+        (posBolaX <= posJogadorX + barraW) &&
+        ((posBolaY + bolaH >= posJogadorY) &&
+            (posBolaY <= posJogadorY + barraH))) {
+        bolaY = (((posBolaY + (bolaH / 2)) -
+            (posJogadorY + (barraH / 2))) / 16);
+        bolaX *= -1;
+    }
+
+    //Colisão com o CPU
+    if (
+        (posBolaX >= posCpuX - barraW) &&
+        ((posBolaY + bolaH >= posCpuY) &&
+            (posBolaY <= posCpuY + barraH))) {
+        bolaY = (((posBolaY + (bolaH / 2)) -
+            (posCpuY + (barraH / 2))) / 16);
+        bolaX *= -1;
+    }
+
+    vBola.style.top = posBolaY + "px";
+    vBola.style.left = posBolaX + "px";
 }
 
 function teclaDw(event) {
@@ -59,7 +93,8 @@ function teclaUp(event) {
 
 function game() {
     if (jogo) {
-        controlaJog();
+        controlaJogo();
+        controlaBola();
     }
 
     frames = requestAnimationFrame(game);
@@ -70,9 +105,15 @@ function iniciaJogo() {
         cancelAnimationFrame(frames);
         jogo = true;
         dirJy = 0;
+        bolaY = 0;
+        if ((Math.random() * 10) < 5) {
+            bolaX = -1;
+        } else {
+            bolaX = 1;
+        }
         posBolaX = posBolaIniX;
-        posBolaY = posBolaIniy;
-        posJogadorY = 0;
+        posBolaY = posBolaIniY;
+        posJogadorY = posJogIniY;
         posCpuY = posCpuIniY;
         game();
     }
