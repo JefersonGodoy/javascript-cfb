51e791e6deaac4206644426bb3c5c917d7020237
Godoy <j.godoysoares@gmail.com>
2022-03-03 13:32:44 -0300

    removendo os tiros

--- aula079/script/jscript.js
+++ aula079/script/jscript.js
@@ -1,38 +1,66 @@
 var diryJ, dirxJ, jog, velJ, pjx, pjy;
+var velT;
 var tamTelaW, tamTelaH;
 var jogo;
 var frames;
-/*
+
 function teclaDw(event) {
-	tecla = event.key;
-	if (tecla == 'ArrowUp') {
+	tecla = event.which;
+	if (tecla == '38') {
 		diryJ = -1;
-	} else if (tecla == 'ArrowDown') {
+	} else if (tecla == '40') {
 		diryJ = 1;
 	}
-	if (tecla == 'ArrowLeft') {
+	if (tecla == '37') {
 		dirxJ = -1;
-	} else if (tecla == 'ArrowRight') {
+	} else if (tecla == '39') {
 		dirxJ = 1;
 	}
-	if (tecla == 'Space') {
+	if (tecla == '32') {
 		//tiro
 	}
 }
 
 function teclaUp(event) {
-	tecla = event.key;
-	if ((tecla == 'ArrowUp') || (tecla == 'ArrowDown')) {
+	tecla = event.which;
+	if ((tecla == '38') || (tecla == '40')) {
 		diryJ = 0;
 	}
-	if ((tecla == 'ArrowLeft') || (tecla == 'ArrowRight')) {
+	if ((tecla == '37') || (tecla == '39')) {
 		dirxJ = 0;
 	}
-	if (tecla == 'Space') {
+	if (tecla == '32') {
 		//tiro
+		atira(pjx + 17, pjy);
 	}
 }
-*/
+
+function atira(x, y) {
+	var t = document.createElement("div");
+	var att1 = document.createAttribute("class");
+	var att2 = document.createAttribute("style");
+	att1.value = "tiroJog";
+	att2.value = "top:" + y + "px; left:" + x + "px";
+	t.setAttributeNode(att1);
+	t.setAttributeNode(att2);
+	document.body.appendChild(t);
+}
+
+function controleTiros() {
+	var tiros = document.getElementsByClassName("tiroJog");
+	var tam = tiros.length;
+	for (var i = 0; i < tam; i++) {
+		if (tiros[i]) {
+			var pt = tiros[i].offsetTop;
+			pt -= velT;
+			tiros[i].style.top = pt + "px";
+			if (pt < 0) {
+				tiros[i].remove();
+			}
+		}
+	}
+}
+
 function controlaJogador() {
 	pjy += diryJ * velJ;
 	pjx += dirxJ * velJ;
@@ -44,6 +72,7 @@ function gameLoop() {
 	if (jogo) {
 		//Funções de controle
 		controlaJogador();
+		controleTiros();
 	}
 	frames.requestAnimationFrame(gameLoop);
 }
@@ -59,7 +88,7 @@ function inicia() {
 	dirxJ = diryJ = 0;
 	pjx = tamTelaW / 2;
 	pjy = tamTelaH / 2;
-	velJ = 5;
+	velJ = velT = 5;
 	jog = document.getElementById("naveJog");
 	jog.style.top = pjy + "px";
 	jog.style.left = pjx + "px";
